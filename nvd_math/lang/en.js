5678d15ab8c56d1d444862e3358d289ccdf1cd70
Dong Nguyen Van <33110542+dongnvsince1999@users.noreply.github.com>
2021-07-17 18:05:07 +0700

    Update code for version 1.0.1

---
+++ nvd_math/lang/en.js
@@ -0,0 +1,14 @@
+
+CKEDITOR.plugins.setLang( 'nvd_math', 'en',
+{
+	title		: 'Công thức toán học',
+	menu    : 'Maths',
+	toolbar	: 'Insert Equation',
+	edit		: 'Edit Equation',
+	button: 'Math',
+	dialogInput: 'Write your TeX here',
+	docLabel: 'TeX documentation',
+	loading: 'loading...',
+	pathName: 'math'
+} );
+
---
+++ nvd_math/lang/vi.js
@@ -0,0 +1,10 @@
+CKEDITOR.plugins.setLang('nvd_math', 'vi', {
+	title: 'Công thức toán học',
+	button: 'Math',
+	dialogInput: 'Nhập mã TeX ở đây',
+	docLabel: 'Tài liệu TeX',
+	loading: 'đang tải...',
+	pathName: 'toán',
+	toolbar	: 'Chèn công thức',
+
+});
